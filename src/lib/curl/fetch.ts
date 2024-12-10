19433b00bded442acd426c3d163977aa310fbcdb
tunan <tunan@gaoding.com>
2024-12-11 02:50:43 +0800

    chore: split code

--- src/lib/curl/fetch.ts
+++ src/lib/curl/fetch.ts
@@ -12,9 +12,10 @@ function fetchDataSync(url: string, options: any): any {
     // 创建共享内存区域，用 Uint8Array 存储字符串（字节数组），并使用 Int32Array 来控制同步
     const sharedArrayBuffer = new SharedArrayBuffer(81920); // 用于存储数据
     const uint8Array = new Uint8Array(sharedArrayBuffer);  // 用于存储字符串数据（字节数组）
+    const int32Array = new Int32Array(sharedArrayBuffer);
 
     const sharedBufferSync = new SharedArrayBuffer(4);  // 用于同步标志（标记数据是否已准备好）
-    const int32Array = new Int32Array(sharedBufferSync);  // 用于控制同步的数组（存储标志位）
+    const syncInt32Array = new Int32Array(sharedBufferSync);  // 用于控制同步的数组（存储标志位）
 
     new Worker(path.join(__dirname, 'thead.js'), {
         workerData: { url, options, sharedArrayBuffer, sharedBufferSync },
@@ -22,10 +23,14 @@ function fetchDataSync(url: string, options: any): any {
 
     // 等待子线程通过 Atomics 通知主线程
     console.log('Waiting for data from worker...');
-    Atomics.wait(int32Array, 0, 0); // 等待子线程写入数据
+    Atomics.wait(syncInt32Array, 0, 0); // 等待子线程写入数据
 
     // 数据准备好后，读取数据（转换为字符串）
-    const responseData = new TextDecoder().decode(uint8Array).trim(); // 从共享内存读取并解码为字符串
+    // 主线程可以读取数据长度并根据它来读取有效数据
+    const dataLength = int32Array[0]; // 获取数据长度
+    const responseData = new TextDecoder()
+        .decode(uint8Array.slice(4, 4 + dataLength))
+        .trim(); // 从共享内存读取并解码为字符串
 
     if(responseData) {
         return {
--- src/lib/curl/thead.ts
+++ src/lib/curl/thead.ts
@@ -4,7 +4,10 @@ import * as https from 'https';
 
 
 const uint8Array = new Uint8Array(workerData.sharedArrayBuffer);
-const int32Array = new Int32Array(workerData.sharedBufferSync);
+const syncInt32Array = new Int32Array(workerData.sharedBufferSync);
+
+const int32Array = new Int32Array(workerData.sharedArrayBuffer);
+
 const { url, options } = workerData;
 
 const request = url.startsWith('https') ?
@@ -23,12 +26,19 @@ const req = request(url, options, (res) => {
         const encoder = new TextEncoder();
         const dataBytes = encoder.encode(data);
 
+        // 保存有效数据的长度
+        int32Array[0] = data.length;  // 保存有效数据的长度
+
+        // 将数据存储在共享内存中
+        uint8Array.set(dataBytes, 4);  // 从第4字节开始存储数据（前4字节用来存储长度）
+
+
         // 将字节数组写入共享内存
         uint8Array.set(dataBytes, 0);
 
         // 通知主线程数据已准备好
-        Atomics.store(int32Array, 0, 1);  // 修改同步标志
-        Atomics.notify(int32Array, 0, 1);  // 通知主线程
+        Atomics.store(syncInt32Array, 0, 1);  // 修改同步标志
+        Atomics.notify(syncInt32Array, 0, 1);  // 通知主线程
     });
 });
 
