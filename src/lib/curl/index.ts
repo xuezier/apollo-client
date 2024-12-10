d6fa3a12cec0afba99f96b244c03176e16d3584f
tunan <tunan@gaoding.com>
2024-12-11 00:49:40 +0800

    chore: support pkg

--- src/lib/curl/index.ts
+++ src/lib/curl/index.ts
@@ -1,6 +1,7 @@
 import { spawnSync } from 'child_process';
 import { CurlMethods } from './enum/CurlMethods';
 import { ICurlOptions, ICurlResponse } from './interface';
+import curl from '../request';
 
 export * from './enum/CurlMethods';
 export * from './interface';
@@ -19,19 +20,46 @@ export default function request(options: ICurlOptions): ICurlResponse {
 
     const url = options.url;
 
-    const cmd = process.execPath;
-    const result = spawnSync(cmd, ['./child.js', url, '-o', JSON.stringify(options)], {
-        cwd: __dirname
-    });
-    const { stdout } = result;
-    const resultString = stdout.toString();
+    /** 添加 pkg 打包支持 */
+    const isPkg = (<any>process).pkg;
+    if(!isPkg) {
+        const result = spawnSync('node', ['./child.js', url, '-o', JSON.stringify(options)], {
+            cwd: __dirname
+        });
+        const { stdout } = result;
+        const resultString = stdout.toString();
 
-    const response = resultString ? JSON.parse(resultString) : {};
+        const response = resultString ? JSON.parse(resultString) : {};
 
-    return {
-        ...response,
-         isJSON() {
-            return (this.headers ? this.headers['content-type'] as string : '' || '').startsWith('application/json');
-        },
-    } as ICurlResponse;
+        return {
+            ...response,
+            isJSON() {
+                return (this.headers ? this.headers['content-type'] as string : '' || '').startsWith('application/json');
+            },
+        } as ICurlResponse;
+    }
+    else {
+        let response: any = {
+            isJSON() {
+                return (this.headers ? this.headers['content-type'] as string : '' || '').startsWith('application/json');
+            },
+        };
+        curl(url, options).then(result => {
+            Object.assign(response, {
+                body: result.data,
+                headers: result.headers,
+                version: `HTTP/${result.httpVersion}`,
+                status: result.statusCode,
+                message: result.statusMessage,
+            })
+        });
+
+        let now = Date.now();
+        while(true) {
+            if((Date.now() - now) > 500)
+                break;
+        }
+
+        return response;
+    }
 }
